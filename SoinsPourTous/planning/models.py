20f2a42b220def9a7eef70703dcc1959064a45f7
Nourboudhina <nourboudhina19@gmail.com>
2024-04-25 15:32:18 +0100

    first commit

--- SoinsPourTous/planning/models.py
+++ SoinsPourTous/planning/models.py
@@ -2,7 +2,7 @@
 from django.db import models
    
 class RendezVous(models.Model) : 
-    date_rendez_vous = models.DateField()
+    date_rendez_vous = models.DateTimeField()
     patient = models.CharField(max_length =  100)
     medecin = models.CharField(max_length =  100)
 
